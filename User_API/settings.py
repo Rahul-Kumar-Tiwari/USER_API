03a69eebe2cac2ffa7216e6ae8357a40853a042c
Rahul Kumar Tiwari <31995292+Rahul-Kumar-Tiwari@users.noreply.github.com>
2020-08-07 13:11:39 +0530

    Update settings.py

--- User_API/settings.py
+++ User_API/settings.py
@@ -94,8 +94,8 @@ DATABASES = {
 }
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_PORT=587
-EMAIL_HOST_USER='rt56277@gmail.com'
-EMAIL_HOST_PASSWORD='rahul56277'
+EMAIL_HOST_USER='abc@gmail.com'
+EMAIL_HOST_PASSWORD='abcd'
 EMAIL_USE_TLS=True
 
 
